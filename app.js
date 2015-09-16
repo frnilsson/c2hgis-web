f376fb346acfe974035f616b9fc65d03ea882e6f
Brian Funk <bfunk@nciinc.com>
2015-09-16 18:54:51 -0400

    updated gitignore, added config, default port, update package

--- app.js
+++ app.js
@@ -19,12 +19,19 @@ var morgan = require('morgan');
 //var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var configEnv = require('./config/env.json');
 
 // **********************************************************
 // config
 
-//var config_app_port = process.env.NODE_APP_PORT;
-//var config_db_url = process.env.NODE_DB_URL; 
+var NODE_ENV = process.env.NODE_ENV;
+console.log('NODE_ENV : '+ NODE_ENV );
+
+var NODE_PORT =  process.env.PORT || configEnv[NODE_ENV].NODE_PORT;
+console.log('NODE_PORT : '+ NODE_PORT );
+
+var GS_PATH = configEnv[NODE_ENV].GS_PATH;
+console.log('GS_PATH : '+ GS_PATH );
 
 // **********************************************************
 // console start
@@ -36,6 +43,8 @@ console.log('package_json.description : '+ package_json.description );
 //console.log('config_app_port : '+ config_app_port );
 //console.log('config_db_url : '+ config_db_url );
 
+// **********************************************************
+// logging
 
 var logDirectory = __dirname + '/log';
 
@@ -62,7 +71,7 @@ app.use('/', express.static(__dirname + '/client'));
 // **********************************************************
 // server
 
-var server = app.listen(8000, function () {
+var server = app.listen(NODE_PORT, function () {
 
   var host = server.address().address;
   var port = server.address().port;

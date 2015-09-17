d8dcdab522fea6723123c8c9eb94774e438d59c8
Laxmisekar Pendem <lpendem@nciinc.com>
2015-09-17 13:50:29 -0400

    mockup updated with new layers

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ var layerType = "state";
 
 function createMap() {
  
- 
      L.mapbox.accessToken = 'pk.eyJ1IjoiY29tcHV0ZWNoIiwiYSI6InMyblMya3cifQ.P8yppesHki5qMyxTc2CNLg';
      map = L.mapbox.map('map', 'fcc.k74ed5ge', {
              attributionControl: true,
@@ -49,6 +48,190 @@ function createMap() {
          transparent: true,
          layers: 'c2hgis:score_pcp_and_provider_state'
      });
+     
+     
+    var wms_state_score_health_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_health_and_broadband_quad'
+     }); 
+     
+    var wms_state_score_premature_death_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_premature_death_and_broadband_quad'
+     });
+     
+     var wms_state_score_adult_obesity_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_adult_obesity_and_broadband_quad'
+     });
+     
+     var wms_county_score_health_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_health_and_broadband_quad'
+     }); 
+     
+    var wms_county_score_premature_death_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_premature_death_and_broadband_quad'
+     });
+     
+     var wms_county_score_adult_obesity_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_adult_obesity_and_broadband_quad'
+     });
+     
+     
+     var wms_state_score_health_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_health_and_broadband'
+     });
+     
+    var wms_state_score_premature_death_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_premature_death_and_broadband'
+     });
+     
+     var wms_state_score_adult_obesity_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_score_adult_obesity_and_broadband'
+     });
+     
+     
+     var wms_county_score_health_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_health_and_broadband'
+     }); 
+     
+    var wms_county_score_premature_death_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_premature_death_and_broadband'
+     });
+     
+     var wms_county_score_adult_obesity_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_score_adult_obesity_and_broadband'
+     });
+     
+    var wms_state_pct_broadband_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_pct_broadband_percentile'
+     });
+     
+    var wms_county_pct_broadband_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_pct_broadband_percentile'
+     });
+     
+    var wms_state_pct_fair_health_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_pct_fair_health_percentile'
+     });
+     
+    var wms_county_pct_fair_health_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_pct_fair_health_percentile'
+     });
+     
+    var wms_state_premature_death_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_premature_death_pct_percentile'
+     });
+     
+    var wms_county_premature_death_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_premature_death_pct_percentile'
+     });
+     
+    var wms_state_adult_obesity_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:state_adult_obesity_pct_percentile'
+     });
+     
+    var wms_county_adult_obesity_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:county_adult_obesity_pct_percentile'
+     });
+     
+     
+     var wms_score_health_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_health_and_broadband'
+     });
+    
+    var wms_score_health_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_health_and_broadband_quad'
+     });
+     
+    var wms_score_premature_death_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_premature_death_and_broadband_quad'
+     });
+     
+    var wms_score_adult_obesity_and_broadband_quad = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_adult_obesity_and_broadband_quad'
+     });
+     
+     var wms_score_premature_death_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_premature_death_and_broadband'
+     });
+     
+     var wms_score_adult_obesity_and_broadband = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:score_adult_obesity_and_broadband'
+     });
+     
+    var wms_pct_broadband_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:pct_broadband_percentile'
+     });
+     
+    var wms_pct_fair_health_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:pct_fair_health_percentile'
+     });
+     
+     var wms_premature_death_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:premature_death_pct_percentile'
+     });
+     
+    var wms_adult_obesity_pct_percentile = L.tileLayer.wms('http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com/c2hgis/wms?', {
+         format: 'image/png',
+         transparent: true,
+         layers: 'c2hgis:adult_obesity_pct_percentile'
+     });
 
      L.control.scale({
          position: 'bottomright'
@@ -61,9 +244,44 @@ function createMap() {
          'Satellite': baseSatellite,
          'Terrain': baseTerrain
      }, {
+     
+       'State+County Health and Broadband (100 colors)': wms_score_health_and_broadband,
+       'State+County Health and Broadband (4 colors)': wms_score_health_and_broadband_quad,
+       'State+County Premature Death and Broadband (4 colors)': wms_score_premature_death_and_broadband_quad,
+       'State+County Adult Obesity and Broadband (4 colors)': wms_score_adult_obesity_and_broadband_quad,
+        'State+County Premature Death and Broadband (100 colors)': wms_score_premature_death_and_broadband,
+        'State+County Adult Obesity and Broadband (100 colors)': wms_score_adult_obesity_and_broadband,
+        'State+County Broadband Access (100 colors)': wms_pct_broadband_percentile,
+        'State+County Fair Health (100 colors)': wms_pct_fair_health_percentile,
+        'State+County Premature Death (100 colors)': wms_premature_death_pct_percentile,
+        'State+County Adult Obesity (100 colors)': wms_adult_obesity_pct_percentile.addTo(map),
+     
+        'State Health and Broadband (4 colors)': wms_state_score_health_and_broadband_quad,
+        'State Premature Death and Broadband (4 colors)': wms_state_score_premature_death_and_broadband_quad,
+        'State Adult Obesity and Broadband (4 colors)': wms_state_score_adult_obesity_and_broadband_quad,
+        'County Health and Broadband (4 colors)': wms_county_score_health_and_broadband_quad,
+        'County Premature Death and Broadband (4 colors)': wms_county_score_premature_death_and_broadband_quad,
+        'County Adult Obesity and Broadband (4 colors)': wms_county_score_adult_obesity_and_broadband_quad,
+        
+        'State Health and Broadband (100 colors)': wms_state_score_health_and_broadband,
+        'State Premature Death and Broadband (100 colors)': wms_state_score_premature_death_and_broadband,
+        'State Adult Obesity and Broadband (100 colors)': wms_state_score_adult_obesity_and_broadband,
+        'County Health and Broadband (100 colors)': wms_county_score_health_and_broadband,
+        'County Premature Death and Broadband (100 colors)': wms_county_score_premature_death_and_broadband,
+        'County Adult Obesity and Broadband (100 colors)': wms_county_score_adult_obesity_and_broadband,
+        
+        'State Broadband Access (100 colors)': wms_state_pct_broadband_percentile,
+        'County Broadband Access (100 colors)': wms_county_pct_broadband_percentile,
+        'State Fair Health (100 colors)': wms_state_pct_fair_health_percentile,
+        'County Fair Health (100 colors)': wms_county_pct_fair_health_percentile,
+        'State Premature Death (100 colors)': wms_state_premature_death_pct_percentile,
+        'County Premature Death (100 colors)': wms_county_premature_death_pct_percentile,
+        'State Adult Obesity (100 colors)': wms_state_adult_obesity_pct_percentile,
+        'County Adult Obesity (100 colors)': wms_county_adult_obesity_pct_percentile,
+        
         'Health and Broadband County': wms_health_and_broadband_county,
         'PCP and Provider County': wms_pcp_and_provider_county,
-        'Health and Broadband State': wms_health_and_broadband_state.addTo(map),
+        'Health and Broadband State': wms_health_and_broadband_state,
         'PCP and Provider State': wms_pcp_and_provider_state
 
      }, {

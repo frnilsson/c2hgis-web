f9a8e60aaca034c740112e3c2b53f654ef1555a1
qihangli <qihang.li@yahoo.com>
2016-04-22 13:17:42 -0400

    point to AWS geoserger

--- js/app.js
+++ js/app.js
@@ -10,18 +10,18 @@
 
 /*** Development geoserver ***/
 
-/*
+
 var geo_host = 'http://c2hgis-geoserv-tc-dev01.elasticbeanstalk.com';
 var geo_space = 'c2hgis';
 var geo_output = 'application/json';
-*/
+
 
 /*** ST geoserver ***/
-
+/*
 var geo_host = 'http://kyauk.fcc.gov:8010/geoserver';
 var geo_space = 'fcc';
 var geo_output = 'json';
-
+*/
 
 /*** Production geoserver ***/
 /*

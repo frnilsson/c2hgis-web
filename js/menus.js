4867efdf8cefcd7f5912d502fc69ed72855c94d7
bizui <bizuilize@gmail.com>
2016-07-29 18:30:16 -0400

    replace files from client folder

--- js/menus.js
+++ js/menus.js
@@ -1,49 +1,58 @@
 (function($, document, window, viewport) {
   $(function () {
-    $('.expand-tertiary').click(function (e) {
+
+
+    $('.layer-switch').on('click', 'a', function(e) {
+        var $this = $(this),
+            id = $this.attr('id');
+
         e.preventDefault();
 
-        var parent_menu = $(this).parents('.top-level-menu, .tertiary-menu');
-        var selected_menu = e.target.href.split('#')[1];
+        if (id === 'insights') {
+            $('.list-health-panel').addClass('hide');
+            $('.list-broadband-panel').addClass('hide');            
+            $('.list-insight-panel').removeClass('hide');            
+        }
+        else if (id === 'health') {
+            $('.list-insight-panel').addClass('hide');
+            $('.list-broadband-panel').addClass('hide');            
+            $('.list-health-panel').removeClass('hide'); 
 
-        parent_menu.hide("slide", { direction: "left" }, 500, function(){
-          $("#" + selected_menu).show("slide", { direction: "right"}, 500);
-        });
+			if (map.hasLayer(broadband_layer)) {
+				map.removeLayer(broadband_layer);
+			}
+			if (map.hasLayer(health_layer)) {
+				map.removeLayer(health_layer);
+			}
+			if (map.hasLayer(count_layer)) {
+				map.removeLayer(count_layer);
+			}	
+			
+        }
+        else if (id === 'broadband') {
+            $('.list-health-panel').addClass('hide');
+            $('.list-insight-panel').addClass('hide');            
+            $('.list-broadband-panel').removeClass('hide');   
 
+
+			if (map.hasLayer(broadband_layer)) {
+				map.removeLayer(broadband_layer);
+			}
+			if (map.hasLayer(health_layer)) {
+				map.removeLayer(health_layer);
+			}
+			if (map.hasLayer(count_layer)) {
+				map.removeLayer(count_layer);
+			}		
+
+			
+        }
+
+        $('.layer-switch').find('li').removeClass('active');
+        $this.parent('li').addClass('active');
     });
 
-    $('.collapse-tertiary').click(function (e) {
-        e.preventDefault();
 
-      var parent_menu = $(this).parents('.top-level-menu, .tertiary-menu');
-      var selected_menu = e.target.href.split('#')[1];
-
-      parent_menu.hide("slide", { direction: "right" }, 500, function(){
-        $("#" + selected_menu).show("slide", { direction: "left"}, 500);
-      });
-    });
-
-    // var $navbar = $('.navbar');
-    // $('.navbar-secondary li.dropdown').mouseenter(function(){
-    //   if (!isMobileLayout()) {
-    //     $(this).addClass('open');
-    //   }
-    // }).mouseleave(function(){
-    //   $(this).removeClass('open');
-    // })
-    // $('.navbar-secondary li.dropdown > a.dropdown-toggle').click(function() {
-    //   if (!isMobileLayout()) {
-    //     window.location = this.href;
-    //   }
-    // });
-    
-    // function isMobileLayout() {
-    //   if ($navbar.hasClass('navbar-static-top')) {
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // }
 
   });
 })(jQuery, document, window, ResponsiveBootstrapToolkit);
